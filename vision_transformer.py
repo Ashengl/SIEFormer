d56a1fd9f264818e59be8169c418ecf21d856751
Chunming Li <113506263+Ashengl@users.noreply.github.com>
2023-10-07 16:22:47 +0800

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -315,3 +315,67 @@ def vit_large(patch_size=16, **kwargs):
         patch_size=patch_size, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4,
         qkv_bias=True, **kwargs)
     return model
+
+
+class DINOHead(nn.Module):
+    def __init__(self, in_dim, out_dim, use_bn=False, norm_last_layer=True, nlayers=3, hidden_dim=2048, bottleneck_dim=256):
+        super().__init__()
+        nlayers = max(nlayers, 1)
+        if nlayers == 1:
+            self.mlp = nn.Linear(in_dim, bottleneck_dim)
+        else:
+            layers = [nn.Linear(in_dim, hidden_dim)]
+            if use_bn:
+                layers.append(nn.BatchNorm1d(hidden_dim))
+            layers.append(nn.GELU())
+            for _ in range(nlayers - 2):
+                layers.append(nn.Linear(hidden_dim, hidden_dim))
+                if use_bn:
+                    layers.append(nn.BatchNorm1d(hidden_dim))
+                layers.append(nn.GELU())
+            layers.append(nn.Linear(hidden_dim, bottleneck_dim))
+            self.mlp = nn.Sequential(*layers)
+        self.apply(self._init_weights)
+        self.last_layer = nn.utils.weight_norm(nn.Linear(bottleneck_dim, out_dim, bias=False))
+        self.last_layer.weight_g.data.fill_(1)
+        if norm_last_layer:
+            self.last_layer.weight_g.requires_grad = False
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.mlp(x)
+        x = nn.functional.normalize(x, dim=-1, p=2)
+        x = self.last_layer(x)
+        return x
+
+
+class VisionTransformerWithLinear(nn.Module):
+
+    def __init__(self, base_vit, num_classes=200):
+
+        super().__init__()
+
+        self.base_vit = base_vit
+        self.fc = nn.Linear(768, num_classes)
+
+    def forward(self, x, return_features=False):
+
+        features = self.base_vit(x)
+        features = torch.nn.functional.normalize(features, dim=-1)
+        logits = self.fc(features)
+
+        if return_features:
+            return logits, features
+        else:
+            return logits
+
+    @torch.no_grad()
+    def normalize_prototypes(self):
+        w = self.fc.weight.data.clone()
+        w = torch.nn.functional.normalize(w, dim=1, p=2)
+        self.fc.weight.copy_(w)
